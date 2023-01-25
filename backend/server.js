8ed0a20f65c34e224436b4d77ed392fa6edebe00
WongSinYan <wsyfiona288@gmail.com>
2023-01-25 16:11:38 +0800

    25/1

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,43 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express()
 
-//route
-app.length('/', (reg, res)=> {
-    res.json({msg:'welcome to the app'})
+//middleware
+
+app.use(express.json())
+
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next()
 })
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
+//route
+/*
+app.get('/', (reg, res)=> {
+    res.json({msg:'welcome to the app'})
 })
+*/
+app.use('/api/workouts', workoutRoutes)
+
+
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{})
+
+//listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('listening on port', process.env.PORT)
+    })
+    
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
 
 process.env
