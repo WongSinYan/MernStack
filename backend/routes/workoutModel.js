196881f76555262888ef2ecf4fcdae7c5ba88047
WongSinYan <wsyfiona288@gmail.com>
2023-01-25 22:43:31 +0800

    #5 done

--- backend/routes/workoutModel.js
+++ backend/routes/workoutModel.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -13,9 +14,16 @@ router.get('/:id', (req, res)=>{
 })
 
 //POST a new workout
-router.post('/',(req, res)=>{
+router.post('/', async (req, res)=>{
+    const{title, load, reps} =  req.body
 
-    res.json({msg:'POST a new workout'})
+    try{
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+   // res.json({msg:'POST a new workout'})
 })
 
 //DEL a workout
