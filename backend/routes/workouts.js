afe4edca46c3b453fa7e44949ab6565065ac825d
WongSinYan <wsyfiona288@gmail.com>
2023-01-28 17:46:05 +0800

    28/1

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,7 +2,9 @@ const express = require('express')
 const {
     createWorkout, 
     getWorkouts,
-    getWorkout
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -10,31 +12,17 @@ const router = express.Router()
 //get all workouts
 router.get('/', getWorkouts)
 
-
 //GET a single workout
 router.get('/:id', getWorkout)
 
-
 //POST a new workout
 router.post('/', createWorkout)
-    /*const{title, load, reps} =  req.body
-
-    try{
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-   // res.json({msg:'POST a new workout'})*/
 
 
 //DEL a workout
-router.delete('/:id',(req, res)=>{
-    res.json({msg:'DELETE a workout'})
-})
+router.delete('/:id',deleteWorkout)
 
 //UPDATE a workout
-router.patch('/:id',(req, res)=>{
-    res.json({msg:'UPDATE a workout'})
-})
+router.patch('/:id',updateWorkout)
+
 module.exports = router
